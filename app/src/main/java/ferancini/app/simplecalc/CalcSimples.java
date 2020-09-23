302a8832acbff567afcde6c8d8feeaf858f12017
wallace-f-rosa <wferancini@gmail.com>
2020-09-23 10:20:22 -0300

    tratamento quando os campos estão vazios

--- app/src/main/java/ferancini/app/simplecalc/CalcSimples.java
+++ app/src/main/java/ferancini/app/simplecalc/CalcSimples.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -21,29 +22,33 @@ public class CalcSimples extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calc_simples);
-        this.edtValor1 = (EditText) findViewById(R.id.edtValor1);
-        this.edtValor2 = (EditText) findViewById(R.id.edtValor2);
+        this.edtValor1 = findViewById(R.id.edtValor1);
+        this.edtValor2 = findViewById(R.id.edtValor2);
         this.txtResultado = (TextView) findViewById(R.id.txtResultado);
     }
 
     public void clickOperador(View v){
-        BigDecimal val1 = new BigDecimal(edtValor1.getText().toString());
-        BigDecimal val2 = new BigDecimal(edtValor2.getText().toString());
-
-        String tag = v.getTag().toString();
         String resultado = "O Resultado é : ";
-        if (tag.equals("add")){
-            resultado += val1.add(val2).toString();
+        if(!edtValor1.getText().toString().equals("") && !edtValor2.getText().toString().equals("")){
+            BigDecimal val1 = new BigDecimal(edtValor1.getText().toString());
+            BigDecimal val2 = new BigDecimal(edtValor2.getText().toString());
+
+            String tag = v.getTag().toString();
+
+            if (tag.equals("add")){
+                resultado += val1.add(val2).toString();
+            }
+            else if (tag.equals("sub")){
+                resultado += val1.subtract(val2).toString();
+            }
+            else if (tag.equals("mult")){
+                resultado += val1.multiply(val2).toString();
+            }
+            else {
+                resultado += val1.divide(val2, 5, BigDecimal.ROUND_CEILING).toString();
+            }
         }
-        else if (tag.equals("sub")){
-            resultado += val1.subtract(val2).toString();
-        }
-        else if (tag.equals("mult")){
-            resultado += val1.multiply(val2).toString();
-        }
-        else {
-            resultado += val1.divide(val2).toString();
-        }
+
         txtResultado.setText(resultado);
     }
 }
