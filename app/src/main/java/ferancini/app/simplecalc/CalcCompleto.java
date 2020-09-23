754d6e45e818f6047a1633c63291caef0757df82
wallace-f-rosa <wferancini@gmail.com>
2020-09-23 09:26:22 -0300

    actvity's para telas da calculadora

---
+++ app/src/main/java/ferancini/app/simplecalc/CalcCompleto.java
@@ -0,0 +1,14 @@
+package ferancini.app.simplecalc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalcCompleto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc_completo);
+    }
+}
